97ac87c2fdea8913983bc680bc21be8deed8541f
avonamardba <avonamardba@gmail.com>
2021-10-23 00:39:40 +0300

    Scripted auto-commit on change (2021-10-23 00:39:40) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,13 +1,24 @@
 import org.apache.hadoop.io.Text;
 
 public class Parser {
+    private String airportId;
+    private String airportName;
+
+
     public Parser(String s) {
         String[] words = s.split(",[\"]");
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].replaceAll("\'", "");
         }
-        String airportId = words[0];
-        String airportName = words[1];
+        airportId = words[0];
+        airportName = words[1];
     }
 
+    public String GetAirportId() {
+        return airportId;
+    }
+
+    public String GetAirportName() {
+        return airportName;
+    }
 }
