cf1e35e5d79980930e4edeff5ef8394c51772fb4
avonamardba <avonamardba@gmail.com>
2021-10-23 00:18:28 +0300

    Scripted auto-commit on change (2021-10-23 00:18:28) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,8 +3,9 @@ import org.apache.hadoop.io.Text;
 public class Parser {
     private String AirportId, AirportName;
 
-    public Parser {
-
+    public Parser(String s) {
+        String[] words = s.split(",");
+        AirportId = 
     }
 
 }
