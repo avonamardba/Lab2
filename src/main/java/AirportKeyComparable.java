c11193edf9c59bf46f889d1a7fe71fd8112afafc
avonamardba <avonamardba@gmail.com>
2021-10-22 22:55:05 +0300

    Scripted auto-commit on change (2021-10-22 22:55:05) by gitwatch.sh

--- src/main/java/AirportKeyComparable.java
+++ src/main/java/AirportKeyComparable.java
@@ -1,2 +1,18 @@
-public class AirportKeyComparable {
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
+public abstract class AirportKeyComparable implements WritableComparable<AirportKeyComparable> {
+    int key, type;
+
+    public AirportKeyComparable() {
+        key = 0;
+        type = 0;
+    }
+
+    public AirportKeyComparable(int x, int y) {
+        key = x;
+        type = y;
+    }
+
+    
 }
