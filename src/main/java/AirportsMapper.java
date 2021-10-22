21c0d836ecdb9330d7459b2ba5a66e3a9eb01afc
avonamardba <avonamardba@gmail.com>
2021-10-22 22:49:18 +0300

    Scripted auto-commit on change (2021-10-22 22:49:18) by gitwatch.sh

--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -1,3 +1,6 @@
-public class AirportsMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapred.Mapper;
+
+public class AirportsMapper extends Mapper<LongWritable, Text, Text> {
 
 }
