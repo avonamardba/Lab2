623fbdb82ab8a8fd142ba917129209b8c0391355
avonamardba <avonamardba@gmail.com>
2021-10-22 23:28:26 +0300

    Scripted auto-commit on change (2021-10-22 23:28:25) by gitwatch.sh

--- src/main/java/AirportKeyComparable.java
+++ src/main/java/AirportKeyComparable.java
@@ -6,24 +6,24 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public abstract class AirportKeyComparable implements WritableComparable<AirportKeyComparable> {
-    private int key, value;
+    private int key, val;
 
     public AirportKeyComparable() {
         key = 0;
-        value = 0;
+        val = 0;
     }
 
     public AirportKeyComparable(int x, int y) {
         key = x;
-        value = y;
+        val = y;
     }
 
     public int GetKey() {
         return key;
     }
 
-    public int GetValue() {
-        return value;
+    public int Getval() {
+        return val;
     }
 
     public int Hashcode() {
@@ -37,14 +37,15 @@ public abstract class AirportKeyComparable implements WritableComparable<Airport
     public int CompareWith(AirportKeyComparable z) {
         if (Integer.compare(key, z.key) != 0) {
             return Integer.compare(key, z.key);
-        } else return Integer.compare(value, z.value);
+        } else return Integer.compare(val, z.val);
     }
 
     public void write(DataOutput output) throws IOException {
         output.write(key);
-        output.write(value);
+        output.write(val);
     }
     public void read(DataInput input) throws IOException {
-        
+        key = input.readInt();
+        val = input.readInt();
     }
 }
--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -1,6 +1,8 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportsMapper extends Mapper<LongWritable, Text, Text> {
+public class AirportsMapper extends Mapper<LongWritable, Text, AirportKeyComparable, Text> {
+    
 
 }
