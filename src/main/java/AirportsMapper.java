f1a4d803ea2ed9d53e06bea03c8675d2b3e0edf7
avonamardba <avonamardba@gmail.com>
2021-10-22 23:54:01 +0300

    Scripted auto-commit on change (2021-10-22 23:54:01) by gitwatch.sh

--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -9,10 +9,6 @@ public class AirportsMapper extends Mapper<LongWritable, Text, AirportKeyCompara
 
     }
 
-    public AirportWritable(Text t) {
-        String fileData = t.toString();
-        String[] s = fileData.split(",");
-        d
-    }
+
 }
 }
