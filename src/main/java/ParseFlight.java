22f9dce73b81f89c0368aa3275572aa553c10684
avonamardba <avonamardba@gmail.com>
2021-10-23 00:54:41 +0300

    Scripted auto-commit on change (2021-10-23 00:54:41) by gitwatch.sh

--- src/main/java/ParseFlight.java
+++ src/main/java/ParseFlight.java
@@ -3,12 +3,14 @@ import org.apache.hadoop.io.Text;
 public class ParseFlight {
     private String[] words;
     private String flightId;
+    private String delay;
 
     public ParseFlight(String s) {
         words = s.split(",");
         flightId = words[14];
+        delay = words[18];
         if (!words[18].isEmpty()) {
-            return flightId;            
+            return flightId;
         }
     }
 
