716f1c9b77e9711af4acf7d29d69567b911177b7
avonamardba <avonamardba@gmail.com>
2021-10-22 23:43:58 +0300

    Scripted auto-commit on change (2021-10-22 23:43:58) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -6,7 +6,8 @@ public class AirportWritable {
     public AirportWritable(Text t) {
         String fileData = t.toString();
         String[] s = fileData.split(",");
-        
+        d
+        }
     }
 
 }
