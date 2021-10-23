e9c53a5c4aa45cd1347857fe781a31db275e4404
avonamardba <avonamardba@gmail.com>
2021-10-23 11:53:43 +0300

    Scripted auto-commit on change (2021-10-23 11:53:43) by gitwatch.sh

--- src/main/java/AirportIDPartitioner.java
+++ src/main/java/AirportIDPartitioner.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportIDPartitioner extends Partitioner<AirportKeyComparable, Text> {
 
     @Override
-    public int getPartition(AirportKeyComparable airportKeyComparable, Text text, int i) {
-        return 0;
+    public int getPartition(AirportKeyComparable key, Text value, int i) {
+        return key.Hashcode() % i;
     }
 }
