18b7b10355e0d9c6da20c73f21378b3798fad0a3
avonamardba <avonamardba@gmail.com>
2021-10-23 12:00:44 +0300

    Scripted auto-commit on change (2021-10-23 12:00:44) by gitwatch.sh

--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -8,7 +8,6 @@ public class AirportsMapper extends Mapper<LongWritable, Text, AirportKeyCompara
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         AirportParser parser = new AirportParser(",", "\"");
         int code = parser.getAirportID(value.toString());
-        context.write(new AirportKeyComparable(code, 0), ) {
-        });
+        context.write(new AirportKeyComparable(code, 0), new Text(parser.getName(value.toString())));
     }
 }
--- src/main/java/FlightsMapper.java
+++ src/main/java/FlightsMapper.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FlightsMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightsMapper extends Mapper<LongWritable, Text, AirportKeyComparable, Text> {
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        FlightParser parser = new FlightParser(",", "\"");
+        int code = parser.getAirportID(value.toString());
+        context.write(new AirportKeyComparable(code, 0), new Text(parser.getDelay(value.toString())));
+    }
 }
