569ec6906a92ede1c2e4ff7fc71581a80022a107
avonamardba <avonamardba@gmail.com>
2021-10-23 12:49:04 +0300

    Scripted auto-commit on change (2021-10-23 12:49:04) by gitwatch.sh

--- src/main/java/AirportsMapper.java
+++ src/main/java/AirportsMapper.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AirportKeyComparable, Text> {
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] cols = value.toString().split(",");
-        int code = Integer.parseInt(cols[0].replace("\"", ""));
-        String name = cols[1].replace("\"", "");
+        AirportParser parser = new AirportParser(",", "\"");
+        int code = parser.getAirportID(value.toString());
+        String name = parser.getName(value.toString());
         context.write(new AirportKeyComparable(code, 0), new Text(name));
     }
 }
