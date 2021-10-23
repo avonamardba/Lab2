cdee458d8fa2f67dfb144a652df01a226d9ca02c
avonamardba <avonamardba@gmail.com>
2021-10-23 11:55:26 +0300

    Scripted auto-commit on change (2021-10-23 11:55:26) by gitwatch.sh

--- src/main/java/AirportIDComparator.java
+++ src/main/java/AirportIDComparator.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class AirportIDComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class AirportIDComparator extends WritableComparator {
+    public AirportIDComparator(){
+
+    }
+
+    public int compare(WritableComparable l, WritableComparable r){
+        AirportKeyComparable x = (AirportKeyComparable) l;
+        AirportKeyComparable y = (AirportKeyComparable) r;
+        return x.c
+    }
 }
