c24913b91fe6d196b1bbbc52701ee0ee88fea2a0
avonamardba <avonamardba@gmail.com>
2021-10-23 11:56:52 +0300

    Scripted auto-commit on change (2021-10-23 11:56:52) by gitwatch.sh

--- src/main/java/AirportIDComparator.java
+++ src/main/java/AirportIDComparator.java
@@ -12,5 +12,9 @@ public class AirportIDComparator extends WritableComparator {
         return x.compareKeys(y);
     }
 
-    
+    public int compare(byte[] b1, int s1, int l1, byte b2[], int s2, int l2) {
+        int a = readInt(b1, s1);
+        int b = readInt(b2, s2);
+        return Integer.compare(a, b);
+    }
 }
