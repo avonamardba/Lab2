ae6ad3665de137dba34095c87b23bad2be9be276
avonamardba <avonamardba@gmail.com>
2021-10-23 13:41:41 +0300

    Scripted auto-commit on change (2021-10-23 13:41:41) by gitwatch.sh

--- src/main/java/bigdata/labs/lab2/AirportIDPartitioner.java
+++ src/main/java/bigdata/labs/lab2/AirportIDPartitioner.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class AirportIDPartitioner extends Partitioner<AirportKeyComparable, Text> {
 
     @Override
-    public int getPartition(@org.jetbrains.annotations.NotNull AirportKeyComparable key, Text value, int i) {
+    public int getPartition(AirportKeyComparable key, Text value, int i) {
         return key.hashcode() % i;
     }
 }
