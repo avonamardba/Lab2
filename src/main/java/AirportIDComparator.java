ec5e2b20ea06fb2d1a76f856d53be6259a57fd79
avonamardba <avonamardba@gmail.com>
2021-10-23 12:44:10 +0300

    Scripted auto-commit on change (2021-10-23 12:44:10) by gitwatch.sh

--- src/main/java/AirportIDComparator.java
+++ src/main/java/AirportIDComparator.java
@@ -2,14 +2,8 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportIDComparator extends WritableComparator {
-    public AirportIDComparator(){
+    public AirportIDComparator() {
 
-    }
-
-    public int compare(WritableComparable l, WritableComparable r){
-        AirportKeyComparable x = (AirportKeyComparable) l;
-        AirportKeyComparable y = (AirportKeyComparable) r;
-        return x.compareKeys(y);
     }
 
     public int compare(byte[] b1, int s1, int l1, byte b2[], int s2, int l2) {
@@ -17,4 +11,12 @@ public class AirportIDComparator extends WritableComparator {
         int b = readInt(b2, s2);
         return Integer.compare(a, b);
     }
+
+    public int compare(WritableComparable l, WritableComparable r) {
+        AirportKeyComparable x = (AirportKeyComparable) l;
+        AirportKeyComparable y = (AirportKeyComparable) r;
+        return x.compareKeys(y);
+    }
+
+
 }
